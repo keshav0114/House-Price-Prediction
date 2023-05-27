d8f9f14a48f4aaa154e913470dff652306b5394e
Himanshu Chaurasiya <63852333+freakyLuffy@users.noreply.github.com>
2023-05-28 00:32:13 +0530

    changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-mbanglore = pickle.load(open('banglore_home_prices_model.pkl', 'rb'))
+mbanglore = pickle.load(open('bangalore_home_prices_model.pkl', 'rb'))
 mchennai = pickle.load(open('Chennai_home_prices_model.pkl', 'rb'))
 mkolkata = pickle.load(open('Kolkata_home_prices_model.pkl', 'rb'))
 mdelhi = pickle.load(open('Delhi_home_prices_model.pkl', 'rb'))
@@ -12,31 +12,31 @@ mhyderabad = pickle.load(open('Hyderabad_home_prices_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('cities.html')
+    return render_template('index.html')
 
 @app.route('/bengaluru')
 def bengaluru():
-    return render_template('index.html' , city_name = 'Bengaluru')
+    return render_template('cities.html' , city_name = 'Bengaluru')
 
 @app.route('/hyderabad')
 def hyderabad():
-    return render_template('index.html' , city_name = 'Hyderabad')
+    return render_template('cities.html' , city_name = 'Hyderabad')
 
 @app.route('/kolkata')
 def kolkata():
-    return render_template('index.html' , city_name = 'Kolkata')
+    return render_template('cities.html' , city_name = 'Kolkata')
 
 @app.route('/chennai')
 def chennai():
-    return render_template('index.html' , city_name = 'Chennai')
+    return render_template('cities.html' , city_name = 'Chennai')
 
 @app.route('/mumbai')
 def mumbai():
-    return render_template('index.html' , city_name = 'Mumbai')
+    return render_template('cities.html' , city_name = 'Mumbai')
 
 @app.route('/delhi')
 def delhi():
-    return render_template('index.html' , city_name = 'Delhi')
+    return render_template('cities.html' , city_name = 'Delhi')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -44,11 +44,33 @@ def predict():
     For rendering results on HTML GUI
     '''
     features = [x for x in request.form.values()]
-    cityname = features[0]
-    int_features = features[1:]
-    int_features = [int(x) for x in int_features]
-    print(int_features)   
-    final_features = [np.array(int_features)]
+    #features = [x for x in request.form.values()]
+    cityname = features[5]
+    area = int(features[0])
+    beds = int(features[1])
+    powerbackup = features[2]
+    gasconnection = features[3]
+    airconditioner = features[4]
+
+    # Map powerbackup, gasconnection, and airconditioner to binary values
+    powerbackup = 1 if powerbackup.lower() == 'yes' else 0
+    gasconnection = 1 if gasconnection.lower() == 'yes' else 0
+    airconditioner = 1 if airconditioner.lower() == 'yes' else 0
+  
+     # Map the form values to the corresponding feature names in your CSV file
+    feature_names = ['Area', 'No. of Bedrooms','Powerbackup', 'Gasconnection', 'AC']
+    feature_values = [area, beds, powerbackup, gasconnection, airconditioner]
+
+    # Create a dictionary to map the form values to the CSV feature names
+    feature_dict = dict(zip(feature_names, feature_values))
+
+    # Retrieve the feature values in the same order as in your CSV file
+    final_features = [np.array([feature_dict['Area'],
+                                feature_dict['No. of Bedrooms'],
+                                feature_dict['Powerbackup'],
+                                feature_dict['Gasconnection'],
+                                feature_dict['AC']])]
+
     if(cityname == "Bengaluru"):
         prediction = mbanglore.predict(final_features)
         output = round(prediction[0], 2)
@@ -96,4 +118,4 @@ def predict_api():
     return jsonify(output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=4000)
